df9462684af6735e1dfb2c05a76398dc94d85583
Anthony Gonzalez <shibby75@live.com>
2021-10-17 17:44:21 -0700

    moving folders around

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+// Initialize express
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+// routes file
+require("./routes/routes")(app);
+
+// Setup listener
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
